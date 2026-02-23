90ffa3864c39b7123f16b732c708036d6791b524
Khúc Phương Nam <khucphuongnam2005@gmail.com>
2026-02-23 19:57:14 +0700

    [LeetCode] 66. Plus One
    Problem: https://leetcode.com/problems/plus-one/description/
    
    Performance:

--- solutions/66_plus-one.js
+++ solutions/66_plus-one.js
@@ -1,34 +1,15 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-class Solution {public:    vector<int> plusOne(vector<int>& digits) {        int n = digits.size();        for (int i = n - 1; i >= 0; i--) {            if (digits[i] < 9) {                digits[i]++;                return digits;            }            digits[i] = 0;        }        digits.insert(digits.begin(), 1);        return digits;    }};
-class Solution {public:    vector<int> plusOne(vector<int>& digits) {        int n = digits.size();        for (int i = n - 1; i >= 0; i--) {            if (digits[i] < 9) {                digits[i]++;                return digits;            }            digits[i] = 0;        }        digits.insert(digits.begin(), 1);        return digits;    }};
 class Solution {
 public:
-vector<int> plusOne(vector<int>& digits) {
-int n = digits.size();
-for (int i = n - 1; i >= 0; i--) {
-if (digits[i] < 9) {
-digits[i]++;
-return digits;
-}
-digits[i] = 0;
-}
-digits.insert(digits.begin(), 1);
-return digits;
-}
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
 };
