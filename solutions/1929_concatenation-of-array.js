1c9cf4ddeb2d96db5771556e7acd1dd75b80bd7e
Khúc Phương Nam <khucphuongnam2005@gmail.com>
2026-02-23 21:11:19 +0700

    [LeetCode] 1929. Concatenation of Array
    Problem: https://leetcode.com/problems/concatenation-of-array/description/
    
    Performance:

--- solutions/1929_concatenation-of-array.js
+++ solutions/1929_concatenation-of-array.js
@@ -1,9 +1,9 @@
-for (int i = 0; i < nums.size(); i++) {
-            ans[i] = nums[i];
+class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         vector<int> ans(nums.size() * 2, 0);
-class Solution {
+        for (int i = 0; i < nums.size(); i++) {
+            ans[i] = nums[i];
             ans[nums.size() + i] = nums[i];
         }
         return ans;
