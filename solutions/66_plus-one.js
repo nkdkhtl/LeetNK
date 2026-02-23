113f0553f8a0f8be99dfd85a0597c0bbf8f8ec8f
Khúc Phương Nam <khucphuongnam2005@gmail.com>
2026-02-23 18:49:40 +0700

    [LeetCode] 66. Plus One
    Problem: https://leetcode.com/problems/plus-one/submissions/1928447414/
    
    Performance:
    - Time: 0 ms (100.00%)
    - Memory: 11.59 MB (53.51%)

---
+++ solutions/66_plus-one.js
@@ -0,0 +1,1 @@
+12345678class Solution {public:    vector<int> plusOne(vector<int>& digits) {        int n = digits.size();                for (int i = n - 1; i >= 0; i--) {            if (digits[i] < 9) {                digits[i]++;
