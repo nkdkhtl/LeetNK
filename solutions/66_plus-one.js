8541156dc6f62a1e5676059916f94f6b64c2bc6f
Khúc Phương Nam <khucphuongnam2005@gmail.com>
2026-02-23 19:12:24 +0700

    [LeetCode] 66. Plus One
    Problem: https://leetcode.com/problems/plus-one/
    
    Performance:
    - Time: 0 ms (100.00%)
    - Memory: 11.59 MB (53.51%)

--- solutions/66_plus-one.js
+++ solutions/66_plus-one.js
@@ -1,15 +1,32 @@
-for (int i = n - 1; i >= 0; i--) {
+5
+6
+2
+3
+4
+1
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+·‌·‌·‌·‌·‌·‌·‌·‌for·‌(int·‌i·‌=·‌n·‌-·‌1;·‌i·‌>=·‌0;·‌i--)·‌{
 public:
-    vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
-class Solution {
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
-        }
-        digits.insert(digits.begin(), 1);
-        return digits;
-    }
+·‌·‌·‌·‌vector<int>·‌plusOne(vector<int>&·‌digits)·‌{
+·‌·‌·‌·‌·‌·‌·‌·‌int·‌n·‌=·‌digits.size();
+class·‌Solution·‌{
+·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌if·‌(digits[i]·‌<·‌9)·‌{
+·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌digits[i]++;
+·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌return·‌digits;
+·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌}
+·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌digits[i]·‌=·‌0;
+·‌·‌·‌·‌·‌·‌·‌·‌}
+·‌·‌·‌·‌·‌·‌·‌·‌digits.insert(digits.begin(),·‌1);
+·‌·‌·‌·‌·‌·‌·‌·‌return·‌digits;
+·‌·‌·‌·‌}
 };
